a0c131c3a1886e5bcd5d2158103b6b5e2485bcb9
James <jcoulson95@gmail.com>
2016-12-11 17:34:10 +0000

    Added master and slave code

---
+++ Master/Master.ino
@@ -0,0 +1,17 @@
+/*
+ * BSPD ADC Isolator:
+ * - The function of this system is to gather sensor data from the BSPD sensors (brake pressure and current) and transmit it
+ *    to the data-logger on NRX in such a way that the ground potentials of the datalogger and the bspd are isolated to avoid
+ *    loops which if present could cause significant damage to the devices involved
+ * - This is acheived with an ATTiny25 device collecting and measuring sensor data via its onboard ADC, this is then transmitted
+ *    to the arduino acting as the datalogger via an optoisolator and a custom serial protocol
+ * 
+ * Maste Code:
+ * - This is example code to be implemented on the master device (datalogger) of the BSPD ADC Isolator system
+ */
+
+void setup(){
+}
+
+void loop() {
+}
