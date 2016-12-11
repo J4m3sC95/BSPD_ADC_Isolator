ae61692dd5b465115b0d1697efdaaf221700ff40
James <jcoulson95@gmail.com>
2016-12-11 18:22:16 +0000

    First version of code created, needs to be tested!!

--- Master/Master.ino
+++ Master/Master.ino
@@ -5,13 +5,68 @@
  *    loops which if present could cause significant damage to the devices involved
  * - This is acheived with an ATTiny25 device collecting and measuring sensor data via its onboard ADC, this is then transmitted
  *    to the arduino acting as the datalogger via an optoisolator and a custom serial protocol
+ *    
+ * Custom Serial Protocol:
+ * - Idle state: Clock Pin = HIGH, Data Pin = HIGH
+ * - Start communication: Clock pin goes low, wait for Data Pin to drop
+ * - Data: data is triggered on the rising edge of the clock and should be sampled by the datalogging arduino on the falling 
+ *     edge of the clock
+ * - 3 x 2 byte integers will be trasmitted corresponsing to the current sensor, rear brake pressure and front brake pressure 
+ *    respectively, the data will be transmitted LSB first
  * 
- * Maste Code:
+ * Master Code:
  * - This is example code to be implemented on the master device (datalogger) of the BSPD ADC Isolator system
  */
 
+// pin definitions for BSPD communication (change 0 to desired number)
+#define CLOCK_PIN 0
+#define DATA_PIN  0
+// the half period of the clock in microseconds (can possibly be 0)
+#define CLOCK_DELAY 50
+
+// array to store the data from the bspd
+unsigned int BSPD_data_array[4] = {0,0,0};
+
 void setup(){
+  BSPD_setup();
 }
 
 void loop() {
+  BSPD_get_data(BSPD_data_array);
 }
+
+void BSPD_setup(){
+  pinMode(CLOCK_PIN, OUTPUT);
+  digitalWrite(CLOCK_PIN, HIGH);
+  pinMode(DATA_PIN, INPUT);  
+}
+
+void BSPD_get_data(unsigned int *data){
+  // initialise the data array
+  for(int n = 0; n < 3; n++){
+    data[n] = 0;
+  }
+  // Start Communication
+  // set clock pin low to start communication
+  digitalWrite(CLOCK_PIN, LOW);
+  // wait for data line to go low
+  while(digitalRead(DATA_PIN));
+
+  // read data
+  // loop through each integer
+  for(int n = 0; n < 3; n++){
+    // loop through each bit of the integer
+    for(int m = 0; m < 16; m++){
+      // raise the clock and pause before reading the value
+      digitalWrite(CLOCK_PIN, HIGH);
+      delayMicroseconds(CLOCK_DELAY);
+      data[n] += digitalRead(DATA_PIN) << m;
+      digitalWrite(CLOCK_PIN, LOW);
+      delayMicroseconds(CLOCK_DELAY);
+    }
+  }
+
+  // set clock high to indicate the transmission is finished
+  digitalWrite(CLOCK_PIN, HIGH);
+}
+
--- Slave/Slave.ino
+++ Slave/Slave.ino
@@ -5,13 +5,60 @@
  *    loops which if present could cause significant damage to the devices involved
  * - This is acheived with an ATTiny25 device collecting and measuring sensor data via its onboard ADC, this is then transmitted
  *    to the arduino acting as the datalogger via an optoisolator and a custom serial protocol
+ *    
+ * Custom Serial Protocol:
+ * - Idle state: Clock Pin = HIGH, Data Pin = HIGH
+ * - Start communication: Clock pin goes low, wait for Data Pin to drop
+ * - Data: data is triggered on the rising edge of the clock and should be sampled by the datalogging arduino on the falling 
+ *     edge of the clock
+ * - 3 x 2 byte integers will be trasmitted corresponsing to the current sensor, rear brake pressure and front brake pressure 
+ *    respectively, the data will be transmitted LSB first
  * 
- * Maste Code:
- * - This is code will run on the ATTiny device
+ * Master Code:
+ * - This is the code that will run on the ATTiny device
  */
 
+ // pin definitions for BSPD communication (change 0 to desired number)
+#define CLOCK_PIN 0
+#define DATA_PIN  0
+
+#define CURRENT_SENSOR_PIN        0
+#define PRESSURE_SENSOR_REAR_PIN  0
+#define PRESSURE_SENSOR_FRONT_PIN 0
+
+unsigned int BSPD_data[] = {0,0,0};
+
 void setup(){
+  pinMode(CLOCK_PIN, INPUT);
+  pinMode(DATA_PIN, OUTPUT);  
+  digitalWrite(DATA_PIN, HIGH);
 }
 
 void loop() {
+  // wait for the clock to go low to indicate the start of a transmission
+  while(digitalRead(CLOCK_PIN));
+  // collect adc readings
+  BSPD_data[0] = analogRead(CURRENT_SENSOR_PIN);
+  BSPD_data[1] = analogRead(PRESSURE_SENSOR_REAR_PIN);
+  BSPD_data[2] = analogRead(PRESSURE_SENSOR_FRONT_PIN);
+
+  // lower the data_pin to indicate ready for communication
+  digitalWrite(DATA_PIN, LOW);
+
+  for(int n = 0; n < 3; n ++){
+    for(int m = 0; m < 16; m++){
+      // wait for the clock signal to rise
+      while(!digitalRead(CLOCK_PIN));
+      if(BSPD_data[n] & (1 << m)){
+        digitalWrite(DATA_PIN, HIGH);
+      }
+      else{
+        digitalWrite(DATA_PIN, LOW);
+      }
+      // wait until the clock pin to drop
+      while(digitalRead(CLOCK_PIN));
+    }
+  }
+  // raise data pin
+  digitalWrite(DATA_PIN, HIGH);
 }
